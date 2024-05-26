9456e47583b22b390cf0b1d149301e957b3689eb
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2024-05-26 10:34:58 +0530

    added lazy & suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,39 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { lazy, Suspense, useState } from "react";
 
-import HomePage from "./pages/Home";
-import Students from "./pages/Students";
-import StudentPage from "./pages/StudentPage";
+// import HomePage from "./pages/Home";
+// import Students from "./pages/Students";
+// import StudentPage from "./pages/StudentPage";
+// import AboutUs from "./pages/AboutUs";
 
 import "./App.css";
 import Layout from "./layout/layout";
 import ThemeContext from "./ThemeContext";
-import { useState } from "react";
+
+// Lazy Optimization
+const HomePage = lazy(() => import("./pages/Home"));
+const Students = lazy(() => import("./pages/Students"));
+const StudentPage = lazy(() => import("./pages/StudentPage"));
+const AboutUs = lazy(() => import("./pages/AboutUs"));
 
 const SubApp = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/students" element={<Students />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="/students/:studentId" element={<StudentPage />} />
-          <Route path="/about-us" element={<h1>About Us Page</h1>} />
-        </Route>
-        <Route
-          path="*"
-          element={<h1>Page Not Found, Please check the URL</h1>}
-        />
-      </Routes>
-    </BrowserRouter>
+    <Suspense fallback={<h1>Loading</h1>}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/students" element={<Students />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/students/:studentId" element={<StudentPage />} />
+            <Route path="/about-us" element={<AboutUs />} />
+          </Route>
+          <Route
+            path="*"
+            element={<h1>Page Not Found, Please check the URL</h1>}
+          />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 };
 
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,5 @@
+const AboutUs = () => {
+  return <h1>Sample Application to Learn Routing</h1>;
+};
+
+export default AboutUs;
